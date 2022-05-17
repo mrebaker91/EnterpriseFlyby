c0f79697d0d8614112e6fe3b24b98170ed176e3a
mrebaker91 <87504261+mrebaker91@users.noreply.github.com>
2022-05-16 20:23:59 -0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ export default class EnterpriseFlyby {
 			actor.transform.local.position = { x: Xpos , y: Ypos, z: Zloop } as MRE.Vector3;
 				//first loop scales the enterprise into view in two phases fast then slow
 				while (Yloop < 25) {
-					//await new Promise(f => setTimeout(f, .1));
+					await new Promise(f => setTimeout(f, .1));
 					if (Yloop < 10) {
 						Yloop = Yloop + .001;
 					}
@@ -62,7 +62,7 @@ export default class EnterpriseFlyby {
 				//second loop enterpise goes across world on Z vector
 				while (Zloop < 1200) {
 					//await sleep(1000);
-					//await new Promise(f => setTimeout(f, .1));
+					await new Promise(f => setTimeout(f, .1));
 					Zloop = Zloop + .01;
 					actor.transform.local.position = { x: Xpos , y: Ypos, z: Zloop } as MRE.Vector3;
 				}
